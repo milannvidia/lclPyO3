08b7ed893447f802b0830fd6f407948acb8ed8de
Milan Schollier <milan.schollier@student.kuleuven.be>
2024-08-15 22:48:19 +0200

    :hammer:

--- lclPyProject/lclpy/localsearch/move/__init__.py
+++
@@ -1,5 +0,0 @@
-"""This package contains everything related to move functions, classes who
-are used to alter the state of the problems and are able to generate valid
-"moves" in the neighbourhood.
-
-"""
--- lclPyProject/lclpy/localsearch/vns/__init__.py
+++
@@ -1,3 +0,0 @@
-"""This package contains all classes unique to variable neighbourhood search.
-
-"""
